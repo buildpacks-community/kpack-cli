6be3f6d271662fa711d2039a5b8ce8b8a901c0f7
Danny Joyce <djoyce@pivotal.io>
2020-07-28 14:26:41 -0400

    Add completion command

--- cmd/kp/main.go
+++ cmd/kp/main.go
@@ -54,6 +54,7 @@ Learn more about kpack @ https://github.com/pivotal/kpack`,
 		getStackCommand(clientSetProvider),
 		getStoreCommand(clientSetProvider),
 		getImportCommand(clientSetProvider),
+		getCompletionCommand(),
 	)
 
 	err := rootCmd.Execute()
@@ -224,4 +225,57 @@ func getImportCommand(clientSetProvider k8s.ClientSetProvider) *cobra.Command {
 	storeFactory := &clusterstore.Factory{Uploader: bpUploader}
 
 	return importcmds.NewImportCommand(clientSetProvider, storeFactory, stackFactory)
+}
+
+func getCompletionCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "completion [bash|zsh|fish|powershell]",
+		Short: "Generate completion script",
+		Long: `To load completions:
+
+Bash:
+
+$ source <(kp completion bash)
+
+# To load completions for each session, execute once:
+Linux:
+  $ kp completion bash > /etc/bash_completion.d/kp
+MacOS:
+  $ kp completion bash > /usr/local/etc/bash_completion.d/kp
+
+Zsh:
+
+# If shell completion is not already enabled in your environment you will need
+# to enable it.  You can execute the following once:
+
+$ echo "autoload -U compinit; compinit" >> ~/.zshrc
+
+# To load completions for each session, execute once:
+$ kp completion zsh > "${fpath[1]}/_kp"
+
+# You will need to start a new shell for this setup to take effect.
+
+Fish:
+
+$ kp completion fish | source
+
+# To load completions for each session, execute once:
+$ kp completion fish > ~/.config/fish/completions/kp.fish
+`,
+		DisableFlagsInUseLine: true,
+		ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
+		Args:                  cobra.ExactValidArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			switch args[0] {
+			case "bash":
+				cmd.Root().GenBashCompletion(os.Stdout)
+			case "zsh":
+				cmd.Root().GenZshCompletion(os.Stdout)
+			case "fish":
+				cmd.Root().GenFishCompletion(os.Stdout, true)
+			case "powershell":
+				cmd.Root().GenPowerShellCompletion(os.Stdout)
+			}
+		},
+	}
 }
