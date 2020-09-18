fa819a3297b7234ec6f69ded0b8395472713cd5a
Tyler Phelan <tphelan@pivotal.io>
2020-09-18 16:04:01 -0400

    Handle jar edge cases more gracefully
    - write zip file directly to tar file to fix issue with file and directory
      sharing the same name https://github.com/pivotal/kpack/issues/468
    - set file modes to 0777 for FAT formatted jars https://github.com/pivotal/kpack/issues/469

--- pkg/archive/zip_test.go
+++ pkg/archive/zip_test.go
@@ -4,7 +4,9 @@
 package archive_test
 
 import (
+	"archive/tar"
 	"archive/zip"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -76,6 +78,70 @@ func testZip(t *testing.T, when spec.G, it spec.S) {
 			assert.Contains(t, files, filepath.Join(tempDir, "file3.txt"))
 		})
 	})
+
+	when("#ZipToTar", func() {
+		it("writes the zip file as a tar file", func() {
+			file, err := ioutil.TempFile("", "file.zip")
+			require.NoError(t, err)
+
+			tempDir, err := ioutil.TempDir("", "test")
+			require.NoError(t, err)
+
+			defer os.Remove(file.Name())
+			defer os.RemoveAll(tempDir)
+
+			createZip(file, t, err)
+
+			tarFile, err := archive.ZipToTar(file.Name())
+			require.NoError(t, err)
+			defer os.RemoveAll(tarFile)
+
+			expectedFiles := []string{"file1.txt", "file2.txt", "file3.txt"}
+			count := 0
+			err = checkTar(tarFile, func(header *tar.Header) {
+				assert.Contains(t, expectedFiles, header.Name)
+				count += 1
+			})
+			require.NoError(t, err)
+			require.Equal(t, len(expectedFiles), count, "tar did not contain the expected number of files")
+		})
+
+		it("writes the tar to the dest dir with 0777 when files are compressed in fat (MSDOS) format", func() {
+			zipFile := filepath.Join("testdata", "fat-zip-to-tar.zip")
+
+			tarFile, err := archive.ZipToTar(zipFile)
+			require.NoError(t, err)
+			defer os.RemoveAll(tarFile)
+
+			count := 0
+			err = checkTar(tarFile, func(header *tar.Header) {
+				assert.Equal(t, int64(0777), header.Mode)
+				count += 1
+			})
+			require.NoError(t, err)
+			require.Equal(t, 1, count, "tar did not contain the expected number of files")
+		})
+	})
+}
+
+func checkTar(tarPath string, checkHeader func(header *tar.Header)) error {
+	f, err := os.Open(tarPath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	tr := tar.NewReader(f)
+	for {
+		header, err := tr.Next()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		checkHeader(header)
+	}
+	return nil
 }
 
 func createZip(file *os.File, t *testing.T, err error) {
