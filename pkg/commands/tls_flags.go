d563b15fd1e22883179ea49be04f6a4e0320fbf5
Tyler Phelan <tphelan@pivotal.io>
2020-09-29 14:50:09 -0400

    Remove extra default message

--- pkg/commands/tls_flags.go
+++ pkg/commands/tls_flags.go
@@ -8,5 +8,5 @@ import (
 
 func SetTLSFlags(cmd *cobra.Command, cfg *registry.TLSConfig) {
 	cmd.Flags().StringVar(&cfg.CaCertPath, "registry-ca-cert-path", "", "add CA certificates for registry API (format: /tmp/ca.crt)")
-	cmd.Flags().BoolVar(&cfg.VerifyCerts, "registry-verify-certs", true, "set whether to verify server's certificate chain and host name (default true)")
+	cmd.Flags().BoolVar(&cfg.VerifyCerts, "registry-verify-certs", true, "set whether to verify server's certificate chain and host name")
 }
