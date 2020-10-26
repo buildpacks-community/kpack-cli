a3d0bd5f3aa481936c9bfdf135f5db200de4cb84
Tyler Phelan <tphelan@pivotal.io>
2020-10-26 14:22:19 -0400

    Change plural to singular
    - We only support using one CA

--- pkg/commands/command_flags.go
+++ pkg/commands/command_flags.go
@@ -7,7 +7,7 @@ import (
 )
 
 func SetTLSFlags(cmd *cobra.Command, cfg *registry.TLSConfig) {
-	cmd.Flags().StringVar(&cfg.CaCertPath, "registry-ca-cert-path", "", "add CA certificates for registry API (format: /tmp/ca.crt)")
+	cmd.Flags().StringVar(&cfg.CaCertPath, "registry-ca-cert-path", "", "add CA certificate for registry API (format: /tmp/ca.crt)")
 	cmd.Flags().BoolVar(&cfg.VerifyCerts, "registry-verify-certs", true, "set whether to verify server's certificate chain and host name")
 }
 
