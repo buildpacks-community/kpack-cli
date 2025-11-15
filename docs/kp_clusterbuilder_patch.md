8485a4d2004df43268b441409e1df7cdad8459d0
Prakash V <mailprak@gmail.com>
2025-11-15 23:29:34 +0530

    feat: add support for order from : #384

--- docs/kp_clusterbuilder_patch.md
+++ docs/kp_clusterbuilder_patch.md
@@ -6,7 +6,7 @@ Patch an existing cluster builder configuration
 
 Patch an existing clusterbuilder configuration by providing command line arguments.
 
-A buildpack order must be provided with either the path to an order yaml or via the --buildpack flag.
+A buildpack order must be provided with either the path to an order yaml, via the --buildpack flag, or extracted from a builder image using --order-from.
 Multiple buildpacks provided via the --buildpack flag will be added to the same order group.
 
 ```
@@ -18,26 +18,30 @@ kp clusterbuilder patch <name> [flags]
 ```
 kp clusterbuilder patch my-builder --order /path/to/order.yaml --stack tiny --store my-store
 kp clusterbuilder patch my-builder --order /path/to/order.yaml
+kp clusterbuilder patch my-builder --order-from paketobuildpacks/builder-jammy-base
 kp clusterbuilder patch my-builder --buildpack my-buildpack-id --buildpack my-other-buildpack@1.0.1
 ```
 
 ### Options
 
 ```
-  -b, --buildpack strings   buildpack id and optional version in the form of either '<buildpack>@<version>' or '<buildpack>'
-                              repeat for each buildpack in order, or supply once with comma-separated list
-      --dry-run             perform validation with no side-effects; no objects are sent to the server.
-                              The --dry-run flag can be used in combination with the --output flag to
-                              view the Kubernetes resource(s) without sending anything to the server.
-  -h, --help                help for patch
-  -o, --order string        path to buildpack order yaml
-      --output string       print Kubernetes resources in the specified format; supported formats are: yaml, json.
-                              The output can be used with the "kubectl apply -f" command. To allow this, the command
-                              updates are redirected to stderr and only the Kubernetes resource(s) are written to stdout.
-                              The APIVersion of the outputted resources will always be the latest APIVersion known to kp (currently: v1alpha2).
-  -s, --stack string        stack resource to use
-      --store string        buildpack store to use
-  -t, --tag string          registry location where the builder will be created
+  -b, --buildpack strings              buildpack id and optional version in the form of either '<buildpack>@<version>' or '<buildpack>'
+                                         repeat for each buildpack in order, or supply once with comma-separated list
+      --dry-run                        perform validation with no side-effects; no objects are sent to the server.
+                                         The --dry-run flag can be used in combination with the --output flag to
+                                         view the Kubernetes resource(s) without sending anything to the server.
+  -h, --help                           help for patch
+  -o, --order string                   path to buildpack order yaml
+      --order-from string              builder image to extract buildpack order from
+      --output string                  print Kubernetes resources in the specified format; supported formats are: yaml, json.
+                                         The output can be used with the "kubectl apply -f" command. To allow this, the command
+                                         updates are redirected to stderr and only the Kubernetes resource(s) are written to stdout.
+                                         The APIVersion of the outputted resources will always be the latest APIVersion known to kp (currently: v1alpha2).
+      --registry-ca-cert-path string   add CA certificate for registry API (format: /tmp/ca.crt)
+      --registry-verify-certs          set whether to verify server's certificate chain and host name (default true)
+  -s, --stack string                   stack resource to use
+      --store string                   buildpack store to use
+  -t, --tag string                     registry location where the builder will be created
 ```
 
 ### SEE ALSO
--- pkg/commands/clusterbuilder/create.go
+++ pkg/commands/clusterbuilder/create.go
@@ -16,7 +16,9 @@ import (
 	"github.com/buildpacks-community/kpack-cli/pkg/builder"
 	"github.com/buildpacks-community/kpack-cli/pkg/commands"
 	"github.com/buildpacks-community/kpack-cli/pkg/config"
+	"github.com/buildpacks-community/kpack-cli/pkg/dockercreds"
 	"github.com/buildpacks-community/kpack-cli/pkg/k8s"
+	"github.com/buildpacks-community/kpack-cli/pkg/registry"
 )
 
 const (
@@ -26,7 +28,8 @@ const (
 
 func NewCreateCommand(clientSetProvider k8s.ClientSetProvider, newWaiter func(dynamic.Interface) commands.ResourceWaiter) *cobra.Command {
 	var (
-		flags CommandFlags
+		flags     CommandFlags
+		tlsConfig registry.TLSConfig
 	)
 
 	cmd := &cobra.Command{
@@ -35,14 +38,15 @@ func NewCreateCommand(clientSetProvider k8s.ClientSetProvider, newWaiter func(dy
 		Long: `Create a cluster builder by providing command line arguments.
 The cluster builder will be created only if it does not exist.
 
-A buildpack order must be provided with either the path to an order yaml or via the --buildpack flag.
-Multiple buildpacks provided via the --buildpack flag will be added to the same order group. 
+A buildpack order must be provided with either the path to an order yaml, via the --buildpack flag, or extracted from a builder image using --order-from.
+Multiple buildpacks provided via the --buildpack flag will be added to the same order group.
 
 Tag when not specified, defaults to a combination of the default repository and specified builder name.
 The default repository is read from the "default.repository" key in the "kp-config" ConfigMap within "kpack" namespace.
 `,
 		Example: `kp clusterbuilder create my-builder --order /path/to/order.yaml --stack tiny --store my-store
 kp clusterbuilder create my-builder --buildpack my-buildpack-id --buildpack my-other-buildpack@1.0.1
+kp clusterbuilder create my-builder --order-from paketobuildpacks/builder-jammy-base --stack tiny --store my-store
 kp clusterbuilder create my-builder --tag my-registry.com/my-builder-tag --order /path/to/order.yaml --stack tiny --store my-store
 kp clusterbuilder create my-builder --tag my-registry.com/my-builder-tag --buildpack my-buildpack-id --buildpack my-other-buildpack@1.0.1`,
 		Args:         commands.ExactArgsWithUsage(1),
@@ -61,7 +65,8 @@ kp clusterbuilder create my-builder --tag my-registry.com/my-builder-tag --build
 			name := args[0]
 			ctx := cmd.Context()
 
-			return create(ctx, name, flags, ch, cs, newWaiter(cs.DynamicClient))
+			fetcher := registry.NewDefaultFetcher(tlsConfig)
+			return create(ctx, name, flags, ch, cs, fetcher, newWaiter(cs.DynamicClient))
 		},
 	}
 
@@ -70,7 +75,9 @@ kp clusterbuilder create my-builder --tag my-registry.com/my-builder-tag --build
 	cmd.Flags().StringVar(&flags.store, "store", "", "buildpack store to use")
 	cmd.Flags().StringVarP(&flags.order, "order", "o", "", "path to buildpack order yaml")
 	cmd.Flags().StringSliceVarP(&flags.buildpacks, "buildpack", "b", []string{}, "buildpack id and optional version in the form of either '<buildpack>@<version>' or '<buildpack>'\n  repeat for each buildpack in order, or supply once with comma-separated list")
+	cmd.Flags().StringVar(&flags.orderFrom, "order-from", "", "builder image to extract buildpack order from")
 	commands.SetDryRunOutputFlags(cmd)
+	commands.SetTLSFlags(cmd, &tlsConfig)
 	return cmd
 }
 
@@ -80,9 +87,10 @@ type CommandFlags struct {
 	store      string
 	order      string
 	buildpacks []string
+	orderFrom  string
 }
 
-func create(ctx context.Context, name string, flags CommandFlags, ch *commands.CommandHelper, cs k8s.ClientSet, waiter commands.ResourceWaiter) error {
+func create(ctx context.Context, name string, flags CommandFlags, ch *commands.CommandHelper, cs k8s.ClientSet, fetcher builder.Fetcher, waiter commands.ResourceWaiter) error {
 	kpConfig := config.NewKpConfigProvider(cs.K8sClient).GetKpConfig(ctx)
 
 	if flags.tag == "" {
@@ -115,26 +123,42 @@ func create(ctx context.Context, name string, flags CommandFlags, ch *commands.C
 		},
 	}
 
-	if len(flags.buildpacks) > 0 && flags.order != "" {
-		return fmt.Errorf("cannot use --order and --buildpack together")
+	// Validate that only one order source is provided
+	orderSourceCount := 0
+	if len(flags.buildpacks) > 0 {
+		orderSourceCount++
+	}
+	if flags.order != "" {
+		orderSourceCount++
+	}
+	if flags.orderFrom != "" {
+		orderSourceCount++
+	}
+	if orderSourceCount > 1 {
+		return fmt.Errorf("only one of --order, --buildpack, or --order-from can be specified")
 	}
 
+	// Set the order based on the provided flag
+	var err error
 	if len(flags.buildpacks) > 0 {
 		cb.Spec.Order = builder.CreateOrder(flags.buildpacks)
+	} else if flags.order != "" {
+		cb.Spec.Order, err = builder.ReadOrder(flags.order)
+		if err != nil {
+			return err
+		}
+	} else if flags.orderFrom != "" {
+		keychain := dockercreds.DefaultKeychain
+		cb.Spec.Order, err = builder.ReadOrderFromImage(keychain, fetcher, flags.orderFrom)
+		if err != nil {
+			return err
+		}
 	}
 
 	if flags.store != "" {
 		cb.Spec.Store = corev1.ObjectReference{
 			Name: flags.store,
 			Kind: v1alpha2.ClusterStoreKind,
-		}
-	}
-
-	var err error
-	if flags.order != "" {
-		cb.Spec.Order, err = builder.ReadOrder(flags.order)
-		if err != nil {
-			return err
 		}
 	}
 
